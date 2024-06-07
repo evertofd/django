1b43c4789dfd4d18d90229ca6310c28d92780bf2
Everto Jose Farias Duran <efariasd@falabella.cl>
2024-06-06 21:58:11 -0400

    initial commit

---
+++ productos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Categoria,Producto
+# Register your models here.
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre')
+
+class ProductoAdmin(admin.ModelAdmin):
+    exclude =('creado_en', )
+    list_display = ('id','nombre','stock','puntaje','creado_en')    
+
+admin.site.register(Categoria,CategoriaAdmin)
+admin.site.register(Producto,ProductoAdmin)
+
